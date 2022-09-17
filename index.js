da012748481a16c35abec1f64d1942cb04ca8dcf
Safdar Hussain <safdarse063@gmail.com>
2022-09-17 21:52:21 +0500

    my own api

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+const apiData = require("./data.json");
+
+app.get("/", (req, res) => {
+  res.send("hellow i am live");
+});
+
+app.get("/service", (req, res) => {
+  res.send(apiData);
+});
+
+app.listen(port, () => {
+  console.log("i am live again");
+});
